a9fce8a52d422e0bf59dbebc7928a3d956462cda
Michal-Kawiak <65668423+Michal-Kawiak@users.noreply.github.com>
2021-08-07 14:08:27 +0200

    init

---
+++ STM32CubeIDE/Debug/Application/User/subdir.mk
@@ -0,0 +1,115 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/aspep.c \
+F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/main.c \
+F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/mc_api.c \
+F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/mc_config.c \
+F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/mc_configuration_registers.c \
+F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/mc_interface.c \
+F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/mc_math.c \
+F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/mc_parameters.c \
+F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/mc_tasks.c \
+F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/mcp_config.c \
+F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/motorcontrol.c \
+F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/pwm_curr_fdbk.c \
+F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/register_interface.c \
+F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/regular_conversion_manager.c \
+F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/stm32g4xx_hal_msp.c \
+F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/stm32g4xx_it.c \
+F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/stm32g4xx_mc_it.c \
+../Application/User/syscalls.c \
+../Application/User/sysmem.c \
+F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/usart_aspep_driver.c 
+
+OBJS += \
+./Application/User/aspep.o \
+./Application/User/main.o \
+./Application/User/mc_api.o \
+./Application/User/mc_config.o \
+./Application/User/mc_configuration_registers.o \
+./Application/User/mc_interface.o \
+./Application/User/mc_math.o \
+./Application/User/mc_parameters.o \
+./Application/User/mc_tasks.o \
+./Application/User/mcp_config.o \
+./Application/User/motorcontrol.o \
+./Application/User/pwm_curr_fdbk.o \
+./Application/User/register_interface.o \
+./Application/User/regular_conversion_manager.o \
+./Application/User/stm32g4xx_hal_msp.o \
+./Application/User/stm32g4xx_it.o \
+./Application/User/stm32g4xx_mc_it.o \
+./Application/User/syscalls.o \
+./Application/User/sysmem.o \
+./Application/User/usart_aspep_driver.o 
+
+C_DEPS += \
+./Application/User/aspep.d \
+./Application/User/main.d \
+./Application/User/mc_api.d \
+./Application/User/mc_config.d \
+./Application/User/mc_configuration_registers.d \
+./Application/User/mc_interface.d \
+./Application/User/mc_math.d \
+./Application/User/mc_parameters.d \
+./Application/User/mc_tasks.d \
+./Application/User/mcp_config.d \
+./Application/User/motorcontrol.d \
+./Application/User/pwm_curr_fdbk.d \
+./Application/User/register_interface.d \
+./Application/User/regular_conversion_manager.d \
+./Application/User/stm32g4xx_hal_msp.d \
+./Application/User/stm32g4xx_it.d \
+./Application/User/stm32g4xx_mc_it.d \
+./Application/User/syscalls.d \
+./Application/User/sysmem.d \
+./Application/User/usart_aspep_driver.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Application/User/aspep.o: F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/aspep.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/aspep.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/main.o: F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/main.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/main.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/mc_api.o: F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/mc_api.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/mc_api.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/mc_config.o: F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/mc_config.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/mc_config.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/mc_configuration_registers.o: F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/mc_configuration_registers.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/mc_configuration_registers.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/mc_interface.o: F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/mc_interface.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/mc_interface.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/mc_math.o: F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/mc_math.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/mc_math.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/mc_parameters.o: F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/mc_parameters.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/mc_parameters.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/mc_tasks.o: F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/mc_tasks.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/mc_tasks.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/mcp_config.o: F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/mcp_config.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/mcp_config.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/motorcontrol.o: F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/motorcontrol.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/motorcontrol.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/pwm_curr_fdbk.o: F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/pwm_curr_fdbk.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/pwm_curr_fdbk.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/register_interface.o: F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/register_interface.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/register_interface.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/regular_conversion_manager.o: F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/regular_conversion_manager.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/regular_conversion_manager.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/stm32g4xx_hal_msp.o: F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/stm32g4xx_hal_msp.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/stm32g4xx_hal_msp.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/stm32g4xx_it.o: F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/stm32g4xx_it.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/stm32g4xx_it.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/stm32g4xx_mc_it.o: F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/stm32g4xx_mc_it.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/stm32g4xx_mc_it.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/syscalls.o: ../Application/User/syscalls.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/syscalls.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/sysmem.o: ../Application/User/sysmem.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/sysmem.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/usart_aspep_driver.o: F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Src/usart_aspep_driver.c Application/User/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DARM_MATH_CM4 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../../Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc -I../../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/Any/Inc -I../../MCSDK_v5.Y.1/MotorControl/MCSDK/MCLib/G4xx/Inc -I../../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/CMSIS/DSP/Include -Ofast -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Application/User/usart_aspep_driver.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
---
+++ STM32CubeIDE/Debug/Drivers/STM32G4xx_HAL_Driver/stm32g4xx_ll_adc.d
@@ -0,0 +1,2 @@
+Drivers/STM32G4xx_HAL_Driver/stm32g4xx_ll_adc.o: \
+ F:/EssencialSoft/MC_SDK_5.Y.1/uartCompatible/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_ll_adc.c
